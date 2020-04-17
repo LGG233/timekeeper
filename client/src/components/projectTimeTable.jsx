fa523e8416bd69b8628b6db698a06008fca3c927
Fred Astaire <lancebc@thegodardgroup.com>
2020-04-17 12:17:16 -0400

    solved time entry problem

--- client/src/components/projectTimeTable.jsx
+++ client/src/components/projectTimeTable.jsx
@@ -1,100 +1,128 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import API from "../util/API";
 import "./table.css";
-import moment from 'moment';
+import moment from "moment";
 
 // Pseudocode
-// 3) button attached to row for editing that calls up modal 
+// 3) button attached to row for editing that calls up modal
 
 class projectTimeTable extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: "",
-            project_name: "",
-            client_name: "",
-            date_of_service: "",
-            hours: "",
-            desc_of_work: "",
-            data: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      project_name: "",
+      client_name: "",
+      date_of_service: "",
+      hours: "",
+      desc_of_work: "",
+      data: [],
     };
+  }
 
-    componentDidMount() {
-        let id = localStorage.getItem("project_id");
-        API.getProjectTime(id).then(res => {
-            this.setState({
-                data: res.data
-            });
-        })
-    };
+  componentDidMount() {
+    let id = localStorage.getItem("project_id");
+    API.getProjectTime(id).then((res) => {
+      this.setState({
+        data: res.data,
+      });
+    });
+  }
 
-    handleDeleteClick = (id) => {
-        var result = window.confirm("Are you sure you want to delete this time entry?")
-        if (result) {
-            localStorage.setItem("entry_id", id);
-            API.deleteTimeEntry(id).then(res => {
-                this.setState({
-                    data: res.data
-                });
-            })
-            localStorage.removeItem("entry_id");
-            window.location.replace("/projectTimeTable")
-        }
-    };
-
-    addNewProject = (id, name) => {
-        localStorage.setItem("client_id", id);
-        localStorage.setItem("client_name", name);
-        window.location.replace("/Project");
-    };
-
-    enterTimeClick = (id, name, project) => {
-        localStorage.setItem(this.state.data.ProjectId, id);
-        localStorage.setItem(this.state.data.client_name, name);
-        localStorage.setItem(this.state.data.project_name, project);
-        window.location.replace("/Entry");
-    };
-
-    editTime = (id) => {
-        localStorage.setItem("entry_id", id);
-        window.location.replace("/editTimeEntry");
-    };
-
-    render() {
-        return (
-            <div>
-                <h4>Time entries for {localStorage.getItem("client_name")} on {localStorage.getItem("project_name")} project</h4>
-                <button className="btn btn-sm btn-secondary card-btn" onClick={() => this.enterTimeClick(localStorage.getItem("id"), localStorage.getItem("name"), localStorage.getItem("project"))}>Enter Time on this Project</button>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Date</th>
-                            <th scope="col">Hours</th>
-                            <th scope="col">Work Performed</th>
-                            {/* <th scope="col">Billed?</th> */}
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.data.map(time => (
-                            <tr>
-                                <th scope="row">{moment.utc(time.date_of_service).format("ll")}</th>
-                                <td>{time.hours}</td>
-                                <td>{time.desc_of_work}</td>
-                                {/* <td></td> */}
-                                <td>
-                                    <button className="btn btn-sm btn-secondary card-btn" onClick={() => this.editTime(time.id)}>Edit Entry</button>
-                                    <button className="btn btn-sm btn-secondary card-btn" onClick={() => this.handleDeleteClick(time.id)}>Delete Entry</button>
-                                    {/* <button className="btn btn-sm btn-secondary card-btn" onClick={() => this.enterTimeClick(time.ProjectId, time.client_name, time.project_name)}>Enter Time on this Project</button> */}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        );
+  handleDeleteClick = (id) => {
+    var result = window.confirm(
+      "Are you sure you want to delete this time entry?"
+    );
+    if (result) {
+      localStorage.setItem("entry_id", id);
+      API.deleteTimeEntry(id).then((res) => {
+        this.setState({
+          data: res.data,
+        });
+      });
+      localStorage.removeItem("entry_id");
+      window.location.replace("/projectTimeTable");
     }
+  };
+
+  addNewProject = (id, name) => {
+    localStorage.setItem("client_id", id);
+    localStorage.setItem("client_name", name);
+    window.location.replace("/Project");
+  };
+
+  enterTimeClick = (id, name, project) => {
+    localStorage.setItem(this.state.data.ProjectId, id);
+    localStorage.setItem(this.state.data.client_name, name);
+    localStorage.setItem(this.state.data.project_name, project);
+    window.location.replace("/Entry");
+  };
+
+  editTime = (id) => {
+    localStorage.setItem("entry_id", id);
+    window.location.replace("/editTimeEntry");
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>
+          Time entries for {localStorage.getItem("client_name")} on{" "}
+          {localStorage.getItem("project_name")} project
+        </h4>
+        <button
+          className="btn btn-sm btn-secondary card-btn"
+          onClick={() =>
+            this.enterTimeClick(
+              localStorage.getItem("id"),
+              localStorage.getItem("name"),
+              localStorage.getItem("project")
+            )
+          }
+        >
+          Enter Time on this Project
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Hours</th>
+              <th scope="col">Work Performed</th>
+              {/* <th scope="col">Billed?</th> */}
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((time) => (
+              <tr>
+                <th scope="row">
+                  {moment.utc(time.date_of_service).format("ll")}
+                </th>
+                <td>{time.hours}</td>
+                <td>{time.desc_of_work}</td>
+                {/* <td></td> */}
+                <td>
+                  <button
+                    className="btn btn-sm btn-secondary card-btn"
+                    onClick={() => this.editTime(time.id)}
+                  >
+                    Edit Entry
+                  </button>
+                  <button
+                    className="btn btn-sm btn-secondary card-btn"
+                    onClick={() => this.handleDeleteClick(time.id)}
+                  >
+                    Delete Entry
+                  </button>
+                  {/* <button className="btn btn-sm btn-secondary card-btn" onClick={() => this.enterTimeClick(time.ProjectId, time.client_name, time.project_name)}>Enter Time on this Project</button> */}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default projectTimeTable;
--- client/src/components/time-entry.jsx
+++ client/src/components/time-entry.jsx
@@ -1,135 +1,143 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import API from "../util/API";
 
 class editEntry extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: "",
-            project_name: "",
-            client_name: "",
-            date_of_service: "",
-            hours: "",
-            desc_of_work: "",
-            data: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      project_name: "",
+      client_name: "",
+      date_of_service: "",
+      hours: "",
+      desc_of_work: "",
+      data: [],
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleChange = (event) => {
+    let target = event.target;
+    let name = target.name;
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(localStorage.getItem("client_name"));
+    console.log(this.state);
+    console.log("New time entry posted");
+    let timeData = {
+      entryDate: this.state.entryDate,
+      entryProjectName: localStorage.getItem("project_name"),
+      entryTime: this.state.entryTime,
+      entryDesc: this.state.entryDesc,
+      entryClientName: localStorage.getItem("client_name"),
+      ProjectId: localStorage.getItem("project_id"),
+    };
+    console.log(timeData);
+    this.addTimeEntry(timeData);
+  };
+
+  addTimeEntry = (data, response) => {
+    API.addTimeEntry(data, response)
+      .then((response) => {
+        if (response.statusText === "OK") {
+          window.location.replace("/projectTimeTable");
         }
-    }
+      })
+      .catch((err) => console.log(err));
+  };
 
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
+  handleCancel = (event) => {
+    window.location.replace("/projectTimeTable");
+  };
 
-    handleChange = event => {
-        let target = event.target;
-        let name = target.name;
-        this.setState({
-            [name]: event.target.value
-        });
-    };
-
-    handleSubmit = event => {
-        event.preventDefault();
-        console.log(localStorage.getItem("client_name"));
-        console.log(this.state);
-        console.log("New time entry posted");
-        let timeData = {
-            entryDate: this.state.entryDate,
-            entryProjectName: localStorage.getItem("project_name"),
-            entryTime: this.state.entryTime,
-            entryDesc: this.state.entryDesc,
-            entryClientName: localStorage.getItem("client_name"),
-            ProjectId: localStorage.getItem("project_id")
-        }
-        console.log(timeData);
-        this.addTimeEntry(timeData);
-    };
-
-    addTimeEntry = (data, response) => {
-        API.addTimeEntry(data, response)
-            // .then(data => console.log(data))
-            .then(window.location.replace("/projectTimeTable"))
-            .catch(err => console.log(err))
-    };
-
-    handleCancel = event => {
-        window.location.replace("/projectTimeTable")
-    };
-
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <h4>New Time Entry for {localStorage.getItem("client_name")} on the {localStorage.getItem("project_name")} project</h4>
-                    <div className="container">
-                        <div className="FormCenter">
-                            <form onSubmit={this.handleSubmit} className="FormField">
-                                <div className="FormField">
-                                    <label for="entryDate">Date of Service</label>
-                                    <input
-                                        type="date"
-                                        className="form-control"
-                                        id="entryDate"
-                                        name="entryDate"
-                                        value={this.state.entryDate}
-                                        onChange={this.handleChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label for="entryTime">Time Spent</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="entryTime"
-                                        placeholder="Enter time"
-                                        name="entryTime"
-                                        value={this.state.entryTime}
-                                        onChange={this.handleChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label for="entryDesc">Work Description</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="entryDesc"
-                                        placeholder="Describe Services"
-                                        name="entryDesc"
-                                        value={this.state.entryDesc}
-                                        onChange={this.handleChange} />
-                                </div>
-                                {/* <div className="form-check">
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <h4>
+            New Time Entry for {localStorage.getItem("client_name")} on the{" "}
+            {localStorage.getItem("project_name")} project
+          </h4>
+          <div className="container">
+            <div className="FormCenter">
+              <form onSubmit={this.handleSubmit} className="FormField">
+                <div className="FormField">
+                  <label for="entryDate">Date of Service</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="entryDate"
+                    name="entryDate"
+                    value={this.state.entryDate}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="entryTime">Time Spent</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="entryTime"
+                    placeholder="Enter time"
+                    name="entryTime"
+                    value={this.state.entryTime}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="entryDesc">Work Description</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="entryDesc"
+                    placeholder="Describe Services"
+                    name="entryDesc"
+                    value={this.state.entryDesc}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                {/* <div className="form-check">
                                     <input className="form-check-input" type="radio" name="billed" id="billed" value="option1" unchecked></input>
                                     <label className="form-check-label" for="exampleRadios1">
                                         Billed?</label>
                                 </div> */}
-                                <br></br>
-                                <button
-                                    className="btn btn-sm btn-secondary entrySubmit"
-                                    onClick={this.handleSubmit}
-                                >
-                                    Submit{" "}
-                                </button>
-                                <button
-                                    className="btn btn-sm btn-secondary entryCancel"
-                                    onClick={this.handleCancel}
-                                >
-                                    Cancel{" "}
-                                </button>
-                                <button
-                                    className="btn btn-sm btn-secondary entryView"
-                                    onClick={this.handleCancel}
-                                >
-                                    View Project Time{" "}
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <br></br>
+                <button
+                  className="btn btn-sm btn-secondary entrySubmit"
+                  onClick={this.handleSubmit}
+                >
+                  Submit{" "}
+                </button>
+                <button
+                  className="btn btn-sm btn-secondary entryCancel"
+                  onClick={this.handleCancel}
+                >
+                  Cancel{" "}
+                </button>
+                <button
+                  className="btn btn-sm btn-secondary entryView"
+                  onClick={this.handleCancel}
+                >
+                  View Project Time{" "}
+                </button>
+              </form>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default editEntry;
-
